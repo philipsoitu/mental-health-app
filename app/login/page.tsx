ad8df47a0491609a11943aecaeb314be77abc241
tran-ethan <tran.ethan@hotmail.com>
2024-12-21 19:51:16 -0500

    test

---
+++ app/login/page.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next'
+
+const LoginPage: NextPage = () => {
+  return (
+    <h1>
+        Log in
+    </h1>
+  )
+}
+
+export default LoginPage
+
---
+++ app/signup/page.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next'
+
+const SignupPage: NextPage = () => {
+  return (
+    <h1>
+        Sign up
+    </h1>
+  )
+}
+
+export default SignupPage
+
